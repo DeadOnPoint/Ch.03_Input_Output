dc3edf9423376ab809554e9f23d2b456ad5627d2
jacobsonwil <jacobsonwil@urbandaleschools.com>
2021-09-16 09:44:25 -0500

    ch 3 commit

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -10,3 +10,10 @@ base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
 '''
+
+print("Welcome to trapezoidal calculations!")
+b1 = float(input("What is the first base of the trapezoid? "))
+b2 = float(input("What is the second base of the trapezoid? "))
+h = float(input("What is the height of the trapezoid? "))
+area = ((b1+b2)/2)*h
+print("The area of the trapezoid is", area, "thank you for using our services today!")
