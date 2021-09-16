772916130771047b20971c1e2f1e90a2dbe15528
jacobsonwil <jacobsonwil@urbandaleschools.com>
2021-09-16 10:01:24 -0500

    ch 3 commit

--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -12,3 +12,12 @@ Sem Grade: 86   Final Exam: 52   Exam worth: 15%    Overall: 80.9
 Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
 Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
 '''
+
+print("Let us help you calculate your grade my friend!")
+sem = float(input("What is your semester grade? "))
+fin = float(input("What is your final exam grade? "))
+w = float(input("How much is the final exam worth? "))
+wow = w/100
+duh = 1-wow
+overall = (sem*duh)+(fin*wow)
+print("Your final semester grade is", overall, "%, it was no problem helping bud.")
